23ff0dba5bada02db9b60fd3f3e888f03275463f
Adrian <kontakt@adrianflak.pl>
2025-02-10 18:24:24 +0100

    method POST

--- src/main/java/com/example/demo/controller/MessageControler.java
+++ src/main/java/com/example/demo/controller/MessageControler.java
@@ -6,9 +6,7 @@ import com.example.demo.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MessageControler {
@@ -39,5 +37,10 @@ public class MessageControler {
         messageService.deleteMessage(id);
         return "redirect:/";
     }
-    
+//    @DeleteMapping("/deleteMesage/{id}")
+//    public String deleteMessage(@PathVariable Long id) {
+//        messageService.deleteMessage(id);
+//        return "redirect:/";
+//    }
+
 }
